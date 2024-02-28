d10d81a0720c8ca06bcf34f3646ee545c1fabe26
kri_YamiHikari <1906178267@qq.com>
2024-02-28 09:31:51 +0800

    Initial Commit

---
+++ src/test/java/org/example/musicinfomanager/MusicInfoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.musicinfomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicInfoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
